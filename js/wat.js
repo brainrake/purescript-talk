67f634a122bc31969fea6039a4b23407a94d580b
Márton Boros <martonboros@gmail.com>
2015-10-26 22:31:38 +0100

    eval wat.js

--- js/wat.js
+++ js/wat.js
@@ -1,49 +1,70 @@
 [] + []
+// > ''
 
 [] + {}
+// > '[object Object]'
 
 {} + []
+// > 0
 
 {} + {}
+// > '[object Object][object Object]'
 
 1 + "2"
+// > '12'
 
 [1, 2] + 3
+// > '1,23'
 
 []
+// > []
 
 typeof "str"
+// > 'string'
 
 typeof {}
+// > 'object'
 
 typeof []
+// > 'object'
 
 typeof NaN
+// > 'number'
 
 null != false
+// > true
 
 null != true
+// > true
 
 null == undefined
+// > true
 
 (!!null) == false
+// > true
 
 " " == true
+// > false
 
 0 == false
+// > true
 
 "" == false
+// > true
 
 [""] == false
+// > true
 
 [] == 0
+// > true
 
 
 
 
-/*
+
 
 o = {v: 1, f: function(){console.log(this.v); return null}}
+
 o.f() // method call, sets `this` in `f` to `o`
 f = o.f
 f()   // function call, sets `this` to global object
@@ -56,9 +77,10 @@ setTimeout(o.f.bind(o), 100)
 
 for (var i = 0; i < 5; i++) {
   setTimeout(
-    function(){console.log(i)}
+    function(i){function(){console.log(i)}}(i)
     , 100)
 }
 "done"
 
 */
+// > undefined
